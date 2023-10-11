603f0563f3de88606b20c180c0ef4df63e020e6a
santanapgh <humbertogsantanap@gmail.com>
2023-10-11 00:24:47 -0400

    actualizando index, ccs y formulario

--- js/contacto.js
+++ js/contacto.js
@@ -6,50 +6,85 @@ const opciones = document.getElementById("opciones");
 const mensaje = document.getElementById("message");
 const btn = document.getElementById('btn');
 
+const formulario = document.getElementById("formulario");
+const inputs = document.querySelectorAll("#formulario input");
+
 const expresiones = {
-  nombre: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
-  apellido: /^[a-zA-ZÀ-ÿ\s]{3,40}$/, // Letras y espacios, pueden llevar acentos.
+  nombre: /^[a-zA-ZÀ-ÿ\s]{3,20}$/, // Letras y espacios, pueden llevar acentos.
+  apellido: /^[a-zA-ZÀ-ÿ\s]{3,20}$/, // Letras y espacios, pueden llevar acentos.
   correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
   telefono: /^\d{11,14}$/ // 11 a 14 numeros.
 }
 
+const validarFormulario = (e) => {
+  switch (e.target.name) {
+    case "nombre":
+      if (expresiones.nombre.test(e.target.value)){
+        document.getElementById("form__content-nombre").classList.remove("form_content-incorrecto");
+        document.getElementById("form__content-nombre").classList.add("form_content-correcto");
+        document.querySelector("#form__content-nombre i").classList.remove("fa-circle-check");
+        document.querySelector("#form__content-nombre i").classList.add("fa-circle-xmark");
+        
 
-/*Swal.fire('Bienvenido!') */
+      } else{
+        document.getElementById("form__content-nombre").classList.add("form_content-incorrecto");
+        document.getElementById("form__content-nombre").classList.remove("form_content-correcto");
+        document.querySelector("#form__content-nombre i").classList.add("fa-circle-xmark");
+        document.querySelector("#form__content-nombre i").classList.remove("fa-circle-check"); 
+
+      }
+
+        break;
+  }
+
+}
+
+
+inputs.forEach((input) => {
+  input.addEventListener("keyup", validarFormulario);
+  input.addEventListener("blur", validarFormulario);
+})
 
 function validar() {
 
   if (nombre.value === "") {
     Swal.fire({
       icon: 'error',
-      text: 'Nombre no puede estar vacio'});
+      text: 'Nombre no puede estar vacio'
+    });
   }
 
   else if (apellido.value === "") {
     Swal.fire({
       icon: 'error',
-      text: 'Apellido no puede estar vacio'});
+      text: 'Apellido no puede estar vacio'
+    });
   }
   else if (correo.value === "") {
     Swal.fire({
       icon: 'error',
-      text: 'Ingresa un Correo Eectronico'});
+      text: 'Ingresa un Correo Eectronico'
+    });
   }
   else if (telefono.value === "") {
     Swal.fire({
       icon: 'error',
-      text: 'Ingesa un numero de telefono'});
+      text: 'Ingesa un numero de telefono'
+    });
   }
 
   else if (opciones.value === "") {
     Swal.fire({
       icon: 'error',
-      text: 'selecciona una opcion de contacto'});
+      text: 'selecciona una opcion de contacto'
+    });
   }
-  else if (mensaje.value === "") 
+  else if (mensaje.value === "")
     Swal.fire({
       icon: 'error',
-      text: 'Por favor ingresa un mensaje'});
-  
+      text: 'Por favor ingresa un mensaje'
+    });
+
 }
 
 
@@ -58,7 +93,6 @@ document.addEventListener("click", (e) => {
   if (e.target === btn) {
     e.preventDefault();
     validar();
-
   }
 
 
